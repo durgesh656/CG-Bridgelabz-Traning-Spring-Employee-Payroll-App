84c00d898c947357b239da48bef62f704c2b9282
durgesh656 <durgeshkushwaha656@gmail.com>
2025-02-28 00:19:33 +0530

    [Durgesh Kushwaha] Add: logging level properties added in Employee Payroll App✨

--- employeepayrollapp/src/main/java/com/bridgelabz/employeepayrollapp/EmployeepayrollappApplication.java
+++ employeepayrollapp/src/main/java/com/bridgelabz/employeepayrollapp/EmployeepayrollappApplication.java
@@ -1,13 +1,18 @@
 package com.bridgelabz.employeepayrollapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+@Slf4j
 @SpringBootApplication
 public class EmployeepayrollappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeepayrollappApplication.class, args);
+
+		ApplicationContext context = SpringApplication.run(EmployeepayrollappApplication.class, args);
+		log.info("Employee Payroll App Started in {} environment", context.getEnvironment().getProperty("environment"));
 	}
 
 }
