fa23c410c7c12cc8ae5163ec9bd98d4e3954927a
durgesh656 <durgeshkushwaha656@gmail.com>
2025-02-27 11:25:14 +0530

    [Durgesh Kushwaha] Add: use lombok library to auto generate getter setter in Employee Payroll App✨

--- employeepayrollapp/src/main/java/com/bridgelabz/employeepayrollapp/entities/EmployeeEntity.java
+++ employeepayrollapp/src/main/java/com/bridgelabz/employeepayrollapp/entities/EmployeeEntity.java
@@ -3,7 +3,11 @@ package com.bridgelabz.employeepayrollapp.entities;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "employees")
 public class EmployeeEntity {
@@ -23,27 +27,5 @@ public class EmployeeEntity {
     }
 
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
 }
